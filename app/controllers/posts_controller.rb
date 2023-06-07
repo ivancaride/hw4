455bcbf549ebd4609eea9bc46a3b55fcc28d0f19
ivan caride <ivan.carideferreyra@kellogg.northwestern.edu>
2023-06-07 01:58:07 +0000

    cambios finales

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,19 +15,26 @@ class PostsController < ApplicationController
 
   def create
     if @current_user
-      @post = Post.new
-      @post["title"] = params["post"]["title"]
-      @post["description"] = params["post"]["description"]
-      @post["posted_on"] = params["post"]["posted_on"]
-      @post["place_id"] = params["post"]["place_id"]
-      @post.uploaded_image.attach(params["post"]["uploaded_image"])
-      @post["user_id"] = @current_user["id"]
-      @post.save
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
+    @post["user_id"] = @current_user["id"]
+    @post.save
     else
-      flash["notice"] = "Login first."
+    flash["notice"] = "Login first."
     end
     redirect_to "/places/#{@post["place_id"]}"
-  
   end
 
+  def destroy
+    @post = Post.find_by({ "id" => params["id"] })
+    if @post["user_id"] == @current_user["id"]
+      @post.destroy
+    end
+    redirect_to "/places"
+  end
 end
+
